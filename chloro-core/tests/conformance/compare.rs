20829af3d0f8810574a0dd9e456edde3edc25b65
Louis Maddox <louismmx@gmail.com>
2025-12-05 22:04:12 +0000

    fix: use 2024 edition in testing

--- chloro-core/tests/conformance/compare.rs
+++ chloro-core/tests/conformance/compare.rs
@@ -1,13 +1,16 @@
 use crate::helpers::load_fixture;
 use chloro_core::format_source;
-use rust_format::{Formatter, RustFmt};
+use rust_format::{Formatter, RustFmt, Config, Edition};
 
 #[test]
 fn compare_with_rustfmt_app_state() {
     let code = load_fixture("asterism/app_state");
 
     let chloro_output = format_source(&code);
-    let rustfmt_output = RustFmt::default()
+    let rustfmt = RustFmt::from_config(
+        Config::new_str().edition(Edition::Rust2024)
+    );
+    let rustfmt_output = rustfmt
         .format_str(&code)
         .expect("rustfmt failed");
 
